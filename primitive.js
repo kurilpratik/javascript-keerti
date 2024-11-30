29a49e8dfadf50b5fa23fdd83ed1163c3660b185
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-01 00:31:33 +0530

    Primitive / Value types

---
+++ primitive.js
@@ -0,0 +1,34 @@
+// Primitive / Value Types
+// String, number, boolean, undefined, null, symbol
+// JS -> Dynamic language = You can change the data type of a variable on the go.
+
+// String
+let x = "Pratik";
+console.log(x);
+console.log(typeof(x));
+
+// Number
+let y = 123;
+console.log(y);
+console.log(typeof(y));
+
+let z = 1.5;
+console.log(z);
+console.log(typeof(z));
+
+// Boolean
+let p = true;
+console.log(p);
+console.log(typeof(p));
+
+// Undefined
+let q;
+console.log(q);
+console.log(typeof(q));
+q = "Pratik";
+console.log(typeof(q));
+
+// Null
+let r = null;
+console.log(r);
+console.log(typeof(r));
