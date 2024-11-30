01ac997b68ef96934088462707dbaee79d2b6e04
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-01 00:39:23 +0530

    Reference types - objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+// Reference Types - Objects, Arrays, Functions
+
+let course = {
+    title: "HHLD",
+    description: "Projects",
+    rating: 5
+};
+
+console.log(course);
+console.log(typeof(course));
+
+
+//Accessing individual items of Object
+console.log(course.title);
+console.log(course['title']);
