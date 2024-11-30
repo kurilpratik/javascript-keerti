69345607c96ff1e5e16b30d6e851b00e362358e5
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-01 01:14:23 +0530

    Function in JS

---
+++ functions.js
@@ -0,0 +1,9 @@
+// Functions (Reference type) essentially an object
+
+function createCourse(course){
+    console.log("Creating " + course);
+}
+
+console.log("HLD");
+console.log("LLD");
+console.log("DSA");
