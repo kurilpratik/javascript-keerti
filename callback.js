405a1b4413d9cdefbb5ec7de282fb0464796a595
Pratik Kuril <pratikkuril16@gmail.com>
2024-12-19 22:32:27 +0530

    callback - event queue and event loop

--- callback.js
+++ callback.js
@@ -47,3 +47,15 @@ Initially, there is fetchData in global and after 5 seconds data, error and this
 So, where was this data stored for those 5 seconds? Where is it going?
 
 */
+
+/*
+
+Event Queue
+Event Loop
+
+So basically what happens is there can be a lot of callbacks that are registered, a lot of things that are happening asynchronously, maybe we're getting data from 10 different servers so there are suppose 10 different callbacks that are suppose been registered so you are waiting for too 10 different datas at different times, so now you need to wait as all these operations are running right now and as these operations are completed you need to handle them or you need to call the callback function.
+So because there can be many operations that can be running so event queue and event loop are there in JS environment, what happens is whenever you register a callback, the operation is put into event queue, now as soon as this operation is finished your event loop is going to keep checking if this task is finsihed and as it is finished it is going to take it from the event queue and put it inot callstack or to your scope, then it is called.
+Logically, what might also happen is if we have put timeout for 10 different requests / 10 different operations, so maybe we're handling those operations as well, so now there is a big event queue, although what might happen is if those 5seconds are completed and now that callback function should be called but because there are things in the queue that are yet to be processed and event loop is going to pick then up one by one, it is possible that your callabck (your handledata) is not called exaclty 5 seconds later, it will be called at least 5 seconds later but it can be more than that (6s, 7s later...) 
+So while you are expecting for your callback to be executed 5seconds later, it may not because there are a lot of things happening and what event loop is picking up is first something else and your callback is picked up later. 
+
+*/
